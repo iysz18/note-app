dc855c2667d4bcc5a271638ec3e70698ceb9574d
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-05-25 23:03:12 +0200

    feat: '+ Add Note' button has now eventListener and renders an alert for testing.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "normalize.css";
 import "./style.css";
+import { createNoteCard } from "../modules/dom.js";
+
+const addNoteBtn = document.getElementById("add-note");
+addNoteBtn.addEventListener("click", () => {
+    alert("Buttons works");
+});
