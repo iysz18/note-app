c25f00f13cb8b78c3cb9c90e3cf4df12b9fea647
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-05-26 09:01:51 +0200

    refactory: removed unused lines.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "normalize.css";
-import "./style.css";
-import { createNoteCard } from "../modules/dom.js";
-
-const addNoteBtn = document.getElementById("add-note");
-addNoteBtn.addEventListener("click", () => {
-    alert("Buttons works");
-});
